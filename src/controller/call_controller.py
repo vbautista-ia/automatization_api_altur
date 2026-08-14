c4a2913a54f2174877af1e0930a6ee1af46554e6
VICTOR SAUL BAUTISTA RAMIREZ <78534@corp.mca.com.mx>
2026-08-14 16:50:59 -0600

    add search by product in enpoint call/download and redirect to index in main

--- src/controller/call_controller.py
+++ src/controller/call_controller.py
@@ -7,14 +7,16 @@ from configuration.platforms import Platforms
 from service.call_service import CallService
 
 
-call_router = APIRouter()
+call_router = APIRouter(prefix='/call', tags=['Llamadas'])
 
-@call_router.get('/descargar/')
+@call_router.get('/download/')
 async def descargar(input_start: str, input_end: str, tag: str,
                     plataforma: Literal['BBVA_COBRANZA', 'BBVA_RETARGETING'],
-                    segmento: Literal['SPC_', 'DESPACHO_', 'WELCOME_', 'SERVICE_', 'RTG_'] = Query(default=None), max_records: int = 5):
+                    segmento: Literal['SPC_', 'DESPACHO_', 'WELCOME_', 'SERVICE_', 'RTG_'] = Query(default=None),
+                    max_records: int = 5,
+                    product: str = Query(default=None)):
     call_service = CallService(platform=Platforms[plataforma])
-    response = await call_service.download(input_start, input_end, tag, segmento, max_records)
+    response = await call_service.download(input_start, input_end, tag, segmento, max_records, product)
     headers = { 'Content-Disposition': 'attacment; filename="grabaciones.zip"'}
     return StreamingResponse(
                 response,
