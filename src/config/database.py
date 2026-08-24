f555c8b2763812f15e87cb900e28ac97befbab96
VICTOR SAUL BAUTISTA RAMIREZ <78534@corp.mca.com.mx>
2026-08-24 15:05:53 -0600

    change config db to connect postgresql

--- src/config/database.py
+++ src/config/database.py
@@ -1,3 +1,4 @@
+import json
 from typing import Annotated
 
 from fastapi import Depends
@@ -7,11 +8,21 @@ from sqlalchemy.orm import Session, declarative_base, sessionmaker
 from config.enviroments import get_env
 
 
-DB_URL = get_env('DB_URL')
+USER_DB = get_env("USER_DB")
+PASS_DB = get_env("PASS_DB")
+HOST_DB = get_env("HOST_DB")
+PORT_DB = get_env("PORT_DB")
+NAME_DB = get_env("NAME_DB")
+
+DB_URL = f"postgresql://{USER_DB}:{PASS_DB}@{HOST_DB}:{PORT_DB}/{NAME_DB}"
+
+def json_custom_serializer(obj):
+    return json.dumps(obj, ensure_ascii=False)
 
 engine = create_engine(
     url=DB_URL,
-    connect_args={'check_same_thread': False}
+    json_serializer=json_custom_serializer,
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
