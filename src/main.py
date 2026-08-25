1cac800f49f3de07da1db98eb37f215bf475bafc
VICTOR SAUL BAUTISTA RAMIREZ <78534@corp.mca.com.mx>
2026-08-25 16:59:30 -0600

    remove controller data importer

--- src/main.py
+++ src/main.py
@@ -6,7 +6,6 @@ from fastapi.responses import FileResponse
 from controller.report_controller import router as router_report 
 from controller.campaign_controller import router as router_campaign 
 from controller.call_controller import call_router
-from controller.data_importer_controller import data_router
 
 
 logging.basicConfig(
@@ -20,7 +19,6 @@ app = FastAPI()
 app.include_router(router=router_report)
 app.include_router(router=router_campaign)
 app.include_router(router=call_router)
-app.include_router(router=data_router)
 
 @app.get('/', )
 async def index():
