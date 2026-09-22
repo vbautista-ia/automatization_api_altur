52b90277bd0dd8ae766b1118187499e404d27650
VICTOR SAUL BAUTISTA RAMIREZ <78534@corp.mca.com.mx>
2026-09-22 10:38:31 -0600

    add connection and depend to db nuxiba and service and repository

--- src/config/depends.py
+++ src/config/depends.py
@@ -1,10 +1,12 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-from config.database import get_db
+from config.database import get_db, db_sqlserver
 from repository.data_importer_repository import DataImporterRepository
+from repository.nuxiba_repository import NuxibaRepository
 from repository.report_repository import ReportRepository
 from service.data_importer_service import DataImporterService
+from service.nuxiba_service import NuxibaService
 from service.report_service import ReportService
 
 def get_data_importer_repository(db: Session = Depends(get_db)) -> DataImporterRepository:
@@ -19,3 +21,10 @@ def get_report_repository(db: Session = Depends(get_db)) -> ReportRepository:
 
 def get_report_service(repository: ReportRepository = Depends(get_report_repository)) -> ReportService:
     return ReportService(repository)
+
+
+def get_nuxiba_repository(db: db_sqlserver) -> NuxibaRepository:
+    return NuxibaRepository(db)
+
+def get_nuxiba_service(repository = Depends(get_nuxiba_repository)) -> NuxibaService:
+    return NuxibaService(repository)
